71ca4a6ee50bb3febb3a1aa1b2cb291a58efb05e
Barna Mink <barnamink@gmail.com>
2020-08-21 06:37:22 +0100

    .

--- ht16k3.c
+++ ht16k3.c
@@ -1,3 +1,13 @@
+#include <stddef.h>
+#include <linux/i2c.h>
+#include <linux/i2c-dev.h>
 #include "ht16k3.h"
 
+ht16k3_t *
+ht16k3_init(int busnr, int addr)
+{
 
+/* USE I2C_FUNCS to see what is supported: https://stackoverflow.com/questions/9974592/i2c-slave-ioctl-purpose */
+
+	return NULL;
+}
--- ht16k3.h
+++ ht16k3.h
@@ -10,17 +10,16 @@ typedef struct ht16k3 {
 } ht16k3_t;
 
 
-ht16k3_t *ht16k3_init(void);
+ht16k3_t *ht16k3_init(int, int);
 void ht16k3_uninit(ht16k3_t **);
 
 
 #define HT16K3_LED_OFF	0
 #define HT16K3_LED_ON	1
 
-/*  row, col, on or off */
 int ht16k3_clearleds(ht16k3_t *);
 int ht16k3_getled(ht16k3_t *, int, int);
-int ht16k3_chled(ht16k3_t *, int, int, int);
+int ht16k3_setled(ht16k3_t *, int, int, int); /*  row, col, on or off */
 int ht16k3_refreshleds(ht16k3_t *);
 
 #endif
